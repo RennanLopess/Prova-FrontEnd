d6114331e6082f330ef6f7941cab71e384580b54
Rennan Rentz <rennanrentz@gmail.com>
2025-04-25 19:36:21 -0300

    Criando secção JavaScript, e criando constantes.

---
+++ script.js
@@ -0,0 +1,6 @@
+let tasks = [];
+
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
+const addTaskBtn = document.getElementById("addTaskBtn");
+const pendingCount = document.getElementById("pendingCount");
